03c9358a3dce50d8d5e0f2edd05d67f131d86296
wojciech.kepka <wojtek.kepka@protonmail.com>
2019-08-28 14:23:36 +0200

    Preparation for cargo

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use mobi::Mobi;
-use std::path::Path;
-fn main() {
-    let m = Mobi::init(Path::new("/home/wojtek/Downloads/lotr.mobi"));
-    println!(
-        "{:#?} {:#?} {:#?} {:?}",
-        m.header, m.palmdoc, m.mobi, m.exth
-    );
-}
