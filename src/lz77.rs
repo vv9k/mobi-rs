5f3d11603081a6611e816b525ede6d1948e4feed
wojciechkepka <wojtek.kepka@protonmail.com>
2020-02-25 17:56:48 +0100

    Refactor lz77 to more idiomatic version

--- src/lz77.rs
+++ src/lz77.rs
@@ -8,46 +8,59 @@ pub fn decompress_lz77(data: &[u8]) -> Result<String, std::io::Error> {
     while offset < length {
         let byte = data[offset];
         offset += 1;
-        if byte == 0 {
-            text.push(byte as char);
-        } else if byte <= 8 {
-            if offset + byte as usize <= length {
-                for ch in &data[offset..(offset + byte as usize)] {
-                    text.push(*ch as char);
+        match byte {
+            // The first character is a null which are literal
+            // Chars from range 0x09..=0x7f are also literal
+            0x0 | 0x09..=0x7f => {
+                text.push(byte as char);
+            }
+            // next $byte bytes are also literal
+            0x1..=0x8 => {
+                if offset + byte as usize <= length {
+                    &data[offset..(offset + byte as usize)]
+                        .iter()
+                        .for_each(|ch| {
+                            text.push(*ch as char);
+                        });
+                    offset += byte as usize;
                 }
-                offset += byte as usize;
             }
-        } else if byte <= 0x7f {
-            text.push(byte as char);
-        } else if byte <= 0xbf {
-            offset += 1;
-            if offset > length {
-                return Ok(text.iter().collect::<String>());
-            }
-            reader.set_position((offset - 2) as u64);
-            let mut lz77 = reader.read_u16::<BigEndian>().unwrap();
-            lz77 &= 0x3fff;
-            let lz77length = (lz77 & 0x0007) + 3;
-            let lz77offset = lz77 >> 3;
+            // Data is LZ77-compressed
+            0x80..=0xbf => {
+                offset += 1;
+                if offset > length {
+                    return Ok(text.iter().collect::<String>());
+                }
+                reader.set_position((offset - 2) as u64);
+                let mut lz77 = reader.read_u16::<BigEndian>().unwrap();
 
-            if lz77offset < 1 {
-                return Ok(text.iter().collect::<String>());
-            }
-            for _lz77pos in 0..lz77length {
-                let mut textpos: usize = text.len();
-                if textpos >= lz77offset as usize {
-                    textpos -= lz77offset as usize;
-                } else {
-                    break;
+                lz77 &= 0x3fff; // Leftmost two bits are ID bits and need to be dropped
+                let lz77length = (lz77 & 0x0007) + 3; // Length is  rightmost three bits + 3
+                let lz77offset = lz77 >> 3; // Remaining 11 bits are offset
+
+                if lz77offset < 1 {
+                    // Decompression offset is invalid?
+                    return Ok(text.iter().collect::<String>());
                 }
-                let ch = &text[textpos..=textpos].to_owned();
-                for c in ch {
-                    text.push(*c);
+
+                for _lz77pos in 0..lz77length {
+                    let mut textpos: usize = text.len();
+                    if textpos >= lz77offset as usize {
+                        textpos -= lz77offset as usize;
+                    } else {
+                        break;
+                    }
+                    let ch = &text[textpos..=textpos].to_owned();
+                    for c in ch {
+                        text.push(*c);
+                    }
                 }
             }
-        } else {
-            text.push(' ');
-            text.push((byte ^ 0x80) as char);
+            // 0xc0..= 0xff are single charaters XOR 0x80 preceded by a space
+            _ => {
+                text.push(' ');
+                text.push((byte ^ 0x80) as char);
+            }
         }
     }
     Ok(text.iter().collect::<String>())
