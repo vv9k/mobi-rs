7f6c78239fbfeca5851de1c661fd9cff3610977c
wojciechkepka <wojtek.kepka@protonmail.com>
2020-09-24 20:33:48 +0200

    Update example, add example to docs

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,42 @@
 //! License: [*MIT*](https://github.com/wojciechkepka/mobi-rs/blob/master/license)
 //!
 //! ## Examples
-//! Examples are available on the GitHub repository
+//! ### Access basic info
+//! ```no_run
+//! use mobi::Mobi;
+//! fn main() -> Result<(), std::io::Error> {
+//!     let book = vec![0, 0, 0];
+//!     // You can either create a Mobi struct from a slice
+//!     let m = Mobi::new(&book)?;
+//!     // Or from an instance of io::Read
+//!     let stdin = std::io::stdin();
+//!     let mut handle = stdin.lock();
+//!     let m = Mobi::from_read(&mut handle)?;
+//!     // Or from filesystem
+//!     let m = Mobi::from_path("/some/path/to/book.mobi")?;
+//!
+//!     let empty = "".to_string();
+//!     // Access metadata
+//!     let title = m.title().unwrap_or(&empty);
+//!     let author = m.author().unwrap_or(&empty);
+//!     let publisher = m.publisher().unwrap_or(&empty);
+//!     let desc = m.description().unwrap_or(&empty);
+//!     let isbn = m.isbn().unwrap_or(&empty);
+//!     let pub_date = m.publish_date().unwrap_or(&empty);
+//!     let contributor = m.contributor().unwrap_or(&empty);
+//!
+//!     // Access Headers
+//!     let header = &m.header; // Normal Header
+//!     let pdheader = &m.palmdoc; // PalmDOC Header
+//!     let mheader = &m.mobi; // MOBI Header
+//!     let exth = &m.exth; // Extra Header
+//!
+//!     // Access content
+//!     let content = m.content_as_string();
+//!
+//!     Ok(())
+//! }
+//! ```
 pub(crate) mod book;
 #[cfg(feature = "fmt")]
 mod display;
